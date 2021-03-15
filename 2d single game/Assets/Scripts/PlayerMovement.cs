6b9d97bad4922c558738cd0b5f3b532736093c33
zoheb598 <77964783+zoheb598@users.noreply.github.com>
2021-03-15 23:04:34 +0000

    Added player movement

--- 2d single game/Assets/Scripts/PlayerMovement.cs
+++ 2d single game/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,50 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private LayerMask groundLayers;
+
+    private float moveDir;
+    private Rigidbody2D myRB;
+    private bool canJump;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var moveAxis = Vector3.right * moveDir;
+
+        if (-maxSpeed < myRB.velocity.x && myRB.velocity.x < maxSpeed)
+        {
+            myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
+        }
+
+        if (groundCheck.IsTouchingLayers(groundLayers))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<float>());
-        print("Test");
-        transform.position = Vector3.right * context.ReadValue<float>();
+        moveDir = context.ReadValue<float>();
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<bool>());
+        if (canJump)
+        {
+            myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
     }
 
 }
